b63cb13cec0cdfda91bc40e9c54357d1e5bf79fa
vphilippon <vincent.philippon@usherbrooke.ca>
2013-03-21 15:11:02 -0400

    first

---
+++ select-bd.sql
@@ -0,0 +1,31 @@
+/*
+Select BD
+	Guillaume Harvey
+	Kevin Labrie
+	Mathieu Larocque
+	Vincent Philippon
+*/
+
+SELECT *
+FROM Personne
+;
+
+SELECT * 
+FROM Film
+;
+
+SELECT * 
+FROM RoleFilm
+;
+
+SELECT * 
+FROM Serie
+;
+
+SELECT * 
+FROM Episode
+;
+
+SELECT * 
+FROM RoleEpisode
+;
---
+++ tp5/Tp5Exception.java
@@ -0,0 +1,10 @@
+package tp5;
+
+public class Tp5Exception extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public Tp5Exception(String message) {
+		super(message);
+	}
+}
