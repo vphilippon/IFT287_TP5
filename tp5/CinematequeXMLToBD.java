1384bfe0d82f26a2a897f9cac6c3cf8eb44a0470
vphilippon <vincent.philippon@usherbrooke.ca>
2013-03-28 13:09:46 -0400

    Ajout fct insert bd

--- tp5/CinematequeXMLToBD.java
+++ tp5/CinematequeXMLToBD.java
@@ -2,6 +2,7 @@ package tp5;
 
 import java.io.*;
 import java.sql.*;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -9,11 +10,10 @@ import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 import tp5.Connexion;
 
-
 public class CinematequeXMLToBD extends DefaultHandler {
-    
-    private static Connexion conn;
-    
+
+    private static Connexion         conn;
+
     private static PreparedStatement stmtAjouterFilm;
     private static PreparedStatement stmtAjouterEpisode;
     private static PreparedStatement stmtAjouterPersonne;
@@ -21,35 +21,27 @@ public class CinematequeXMLToBD extends DefaultHandler {
     private static PreparedStatement stmtAjouterRoleFilm;
     private static PreparedStatement stmtAjouterSerie;
 
-    private static Statement stmt;
-
-    private static int niveau = -1;
-    
     public static void main(String argv[]) throws ParserConfigurationException,
-			SAXException, IOException, SQLException {
-		if (argv.length < 5) {
-			System.err
-					.println("Usage: ChargeurTableAttribut <fichier> <serveur> <bd> <userid> <pw> [-v]");
-			System.exit(1);
-		}
+            SAXException, IOException, SQLException {
+        if (argv.length < 5) {
+            System.err.println("Usage: ChargeurTableAttribut <nom-bd> <user-id-bd> <mot-de-passe> <fichier-xml-entree>");
+            System.exit(1);
+        }
 
-		DefaultHandler handler = new CinematequeXMLToBD();
-		// DefaultHandler handler = new DefaultHandler();
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		if (argv.length == 6 && argv[5].equals("-v"))
-			factory.setValidating(true);
-		SAXParser saxParser = factory.newSAXParser();
-		conn = new Connexion(argv[1], argv[2], argv[3], argv[4]);
-                init();
-		conn.setAutoCommit(true);
-		stmt = conn.getConnection().createStatement();
-		saxParser.parse(new File(argv[0]), handler);
-		conn.fermer();
-	}
-    
-    private static void init() throws SQLException{
+        DefaultHandler handler = new CinematequeXMLToBD();
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating(true);
+        SAXParser saxParser = factory.newSAXParser();
+        conn = new Connexion("postgres", argv[1], argv[2], argv[3]);
+        init();
+        conn.setAutoCommit(true);
+        saxParser.parse(new File(argv[4]), handler);
+        conn.fermer();
+    }
+
+    private static void init() throws SQLException {
         stmtAjouterFilm = conn.getConnection().prepareStatement(
-                "INSERT INTO Film (titre, dateSortie, realisateur) VALUES (?, ?, ?)");
+                "INSERT INTO Film (titre, dateSortie, description, duree, realisateur) VALUES (?, ?, ?, ?, ?)");
         stmtAjouterEpisode = conn.getConnection().prepareStatement(
                 "INSERT INTO Episode (titre, titreSerie, anneeSortieSerie,"
                         + " noSaison, noEpisode, description, dateDiffusion)"
@@ -57,106 +49,149 @@ public class CinematequeXMLToBD extends DefaultHandler {
         stmtAjouterPersonne = conn.getConnection().prepareStatement(
                 "INSERT INTO Personne (nom, datenaissance, lieunaissance, sexe) VALUES(?, ?, ?, ?)");
         stmtAjouterRoleEpisode = conn.getConnection().prepareStatement(
-                "INSERT INTO RoleEpisode (nomActeur, roleActeur, titreSerie," 
-                        + " noSaison, noEpisode, anneeSortieSerie)" 
+                "INSERT INTO RoleEpisode (nomActeur, roleActeur, titreSerie,"
+                        + " noSaison, noEpisode, anneeSortieSerie)"
                         + " VALUES (?, ?, ?, ?, ?, ?)");
         stmtAjouterRoleFilm = conn.getConnection().prepareStatement(
                 "INSERT INTO RoleFilm (nomActeur, roleActeur, filmTitre, anneeSortie) VALUES (?, ?, ?, ?)");
         stmtAjouterSerie = conn.getConnection().prepareStatement(
-                "INSERT INTO Serie (titre, anneeSortie, realisateur, description, nbSaison)" 
+                "INSERT INTO Serie (titre, anneeSortie, realisateur, description, nbSaison)"
                         + " VALUES (?, ?, ?, ?, ?)");
-        
+
     }
-    
-     /**
-	 * Cette méthode est appelée par le parser suite à la lecture du marqueur de
-	 * début d'un élément.
-	 */
-	public void startElement(String namespaceURI, String lName, String qName,
-			Attributes attrs) throws SAXException {
-		             
-		if (qName.equals("personne")){
-                    insertPersonne(attrs);
-                } else if(qName.equals("film")) {
-                    insertFilm(attrs);
-                } else if(qName.equals("roleFilm")) {
-                   insertRoleFilm(attrs);
-                } else if(qName.equals("serie")) {
-                    insertSerie(attrs);
-                } else if(qName.equals("episode")) {
-                   insertEpisode(attrs);
-                } else if(qName.equals("roleEpisode")) {
-                    insertRoleEpisode(attrs);
-                }
 
-	}
+    /**
+    * Cette méthode est appelée par le parser suite à la lecture du marqueur de
+    * début d'un élément.
+    */
+    public void startElement(String namespaceURI, String lName, String qName,
+            Attributes attrs) throws SAXException {
 
-	/**
-	 * Cette méthode formatte un énoncé SQL insert pour insérer un
-	 * enregistrement dans la table.
-	 */
-	private static String formatterInsert(String nomTable,
-			DefinitionTable defTable, Attributes attrs) {
-		StringBuffer enonceInsert = new StringBuffer("insert into " + nomTable
-				+ " (");
-		enonceInsert.append(attrs.getQName(0));
-		for (int i = 1; i < attrs.getLength(); i++)
-			enonceInsert.append("," + attrs.getQName(i));
-		enonceInsert.append(") values (");
-		enonceInsert.append(formatterValeur(attrs.getQName(0), attrs
-				.getValue(0), defTable));
-		for (int i = 1; i < attrs.getLength(); i++)
-			enonceInsert.append(","
-					+ formatterValeur(attrs.getQName(i), attrs.getValue(i),
-							defTable));
-		enonceInsert.append(")");
-		return enonceInsert.toString();
-	}
+        if (qName.equals("personne")) {
+            insertPersonne(attrs);
+        } else if (qName.equals("film")) {
+            insertFilm(attrs);
+        } else if (qName.equals("roleFilm")) {
+            insertRoleFilm(attrs);
+        } else if (qName.equals("serie")) {
+            insertSerie(attrs);
+        } else if (qName.equals("episode")) {
+            insertEpisode(attrs);
+        } else if (qName.equals("roleEpisode")) {
+            insertRoleEpisode(attrs);
+        }
+    }
 
-	/**
-	 * Cette méthode formatte la valeur d'un attribut dans l'énoncé insert. Elle
-	 * utilise la définition de la table pour obtenir le type de l'attribut.
-	 */
-	private static String formatterValeur(String nomAttribut,
-			String valeurAttribut, DefinitionTable defTable) {
-		int typeAttribut = defTable.getTypeAttribut(nomAttribut);
-		if (typeAttribut == Constantes.CHAINE)
-			return "'" + valeurAttribut + "'";
-		else if (typeAttribut == Constantes.NOMBRE)
-			return valeurAttribut;
-		else if (typeAttribut == Constantes.DATE)
-			return "to_date('" + valeurAttribut + "','YYYY-MM-DD')";
-		else
-			return "";
-	}
+    private void insertPersonne(Attributes attrs) throws SQLException {
+        String nom = attrs.getValue("nom");
+        Date dateNaissance = Date.valueOf(attrs.getValue("dateNaissance"));
+        String lieuNaissance = attrs.getValue("lieuNaissance");
+        int sexe = Integer.parseInt(attrs.getValue("sexe"));
 
-	/**
-	 * Cette méthode est appelée par le parser suite à la lecture du marqueur de
-	 * la fin d'un élément.
-	 */
-	public void endElement(String namespaceURI, String lName, String qName)
-			throws SAXException {
-		niveau = niveau - 1;
-	}
+        stmtAjouterPersonne.setString(1, nom);
+        stmtAjouterPersonne.setDate(2, dateNaissance);
+        stmtAjouterPersonne.setString(3, lieuNaissance);
+        stmtAjouterPersonne.setInt(4, sexe);
+        stmtAjouterPersonne.executeUpdate();
+    }
 
-	/**
-	 * Cette méthode est appelée par le parser pour les avertissements
-	 */
-	public void warning(SAXParseException e) throws SAXException {
-		System.out.println("Warning : " + e);
-	}
+    private void insertFilm(Attributes attrs) throws SQLException {
+        String titre = attrs.getValue("titre");
+        Date dateSortie = Date.valueOf(attrs.getValue("dateSortie"));
+        String description = attrs.getValue("description");
+        int duree = Integer.parseInt(attrs.getValue("duree"));
+        String realisateur = attrs.getValue("realisateur");
 
-	/**
-	 * Cette méthode est appelée par le parser pour les erreur non fatales
-	 */
-	public void error(SAXParseException e) throws SAXException {
-		System.out.println("Error : " + e);
-	}
+        stmtAjouterFilm.setString(1, titre);
+        stmtAjouterFilm.setDate(2, dateSortie);
+        stmtAjouterFilm.setString(3, description);
+        stmtAjouterFilm.setInt(4, duree);
+        stmtAjouterFilm.setString(5, realisateur);
+        stmtAjouterFilm.executeUpdate();
+    }
 
-	/**
-	 * Cette méthode est appelée par le parser pour les erreur fatales
-	 */
-	public void fatalError(SAXParseException e) throws SAXException {
-		System.out.println("Fatal error : " + e);
-	}
+    private void insertRoleFilm(Attributes attrs) throws SQLException {
+        String nomActeur = attrs.getValue("nomActeur");
+        String roleActeur = attrs.getValue("roleActeur");
+        String filmTitre = attrs.getValue("filmTitre");
+        Date anneeSortie = Date.valueOf(attrs.getValue("anneeSortie"));
+
+        stmtAjouterRoleFilm.setString(1, nomActeur);
+        stmtAjouterRoleFilm.setString(2, roleActeur);
+        stmtAjouterRoleFilm.setString(3, filmTitre);
+        stmtAjouterRoleFilm.setDate(4, anneeSortie);
+        stmtAjouterRoleFilm.executeUpdate();
+    }
+
+    private void insertSerie(Attributes attrs) throws SQLException {
+        String titre = attrs.getValue("titre");
+        Date anneeSortie = Date.valueOf(attrs.getValue("anneeSortie"));
+        String realisateur = attrs.getValue("realisateur");
+        String description = attrs.getValue("description");
+        int nbSaison = Integer.parseInt(attrs.getValue("nbSaison"));
+
+        stmtAjouterSerie.setString(1, titre);
+        stmtAjouterSerie.setDate(2, anneeSortie);
+        stmtAjouterSerie.setString(3, realisateur);
+        stmtAjouterSerie.setString(4, description);
+        stmtAjouterSerie.setInt(5, nbSaison);
+        stmtAjouterSerie.executeUpdate();
+    }
+
+    private void insertEpisode(Attributes attrs) throws SQLException {
+        String titre = attrs.getValue("titre");
+        String titreSerie = attrs.getValue("titreSerie");
+        Date anneeSortieSerie = Date.valueOf(attrs.getValue("anneeSortieSerie"));
+        int noSaison = Integer.parseInt(attrs.getValue("noSaison"));
+        int noEpisode = Integer.parseInt(attrs.getValue("noEpisode"));
+        String description = attrs.getValue("description");
+        Date dateDiffusion = Date.valueOf(attrs.getValue("dateDiffusion"));
+
+        stmtAjouterEpisode.setString(1, titre);
+        stmtAjouterEpisode.setString(2, titreSerie);
+        stmtAjouterEpisode.setDate(3, anneeSortieSerie);
+        stmtAjouterEpisode.setInt(4, noSaison);
+        stmtAjouterEpisode.setInt(5, noEpisode);
+        stmtAjouterEpisode.setString(6, description);
+        stmtAjouterEpisode.setDate(7, dateDiffusion);
+        stmtAjouterEpisode.executeUpdate();
+    }
+
+    private void insertRoleEpisode(Attributes attrs) throws SQLException {
+        String nomActeur = attrs.getValue("nomActeur");
+        String roleActeur = attrs.getValue("roleActeur");
+        String titreSerie = attrs.getValue("titreSerie");
+        int noSaison = Integer.parseInt(attrs.getValue("noSaison"));
+        int noEpisode = Integer.parseInt(attrs.getValue("noEpisode"));
+        Date anneeSortieSerie = Date.valueOf(attrs.getValue("anneeSortieSerie"));
+
+        stmtAjouterRoleEpisode.setString(1, nomActeur);
+        stmtAjouterRoleEpisode.setString(2, roleActeur);
+        stmtAjouterRoleEpisode.setString(3, titreSerie);
+        stmtAjouterRoleEpisode.setInt(4, noSaison);
+        stmtAjouterRoleEpisode.setInt(5, noEpisode);
+        stmtAjouterRoleEpisode.setDate(6, anneeSortieSerie);
+        stmtAjouterRoleEpisode.executeUpdate();
+    }
+
+    /**
+     * Cette méthode est appelée par le parser pour les avertissements
+     */
+    public void warning(SAXParseException e) throws SAXException {
+        System.out.println("Warning : " + e);
+    }
+
+    /**
+     * Cette méthode est appelée par le parser pour les erreur non fatales
+     */
+    public void error(SAXParseException e) throws SAXException {
+        System.out.println("Error : " + e);
+    }
+
+    /**
+     * Cette méthode est appelée par le parser pour les erreur fatales
+     */
+    public void fatalError(SAXParseException e) throws SAXException {
+        System.out.println("Fatal error : " + e);
+    }
 }
